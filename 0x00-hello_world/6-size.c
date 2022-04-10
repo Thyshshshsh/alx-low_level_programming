74c21001a884372a4234ad89ddc35562f324c046
thyshshshsh <nikoroghogho@gmail.com>
2022-04-10 05:00:06 -0700

    How far

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,22 +20,3 @@ int main(void)
 
 	return (0);
 }
-
-100-intel
-#!/bin/bash
-gcc -S -masm=intel $CFILE
-
-101-quote.c
-#include <unistd.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, quote, 59);
-	return (1);
-}
